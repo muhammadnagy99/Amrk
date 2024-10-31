779fdf98750dc53e7219205ad2bfbba7625c6bcd
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-10-31 18:17:26 +0300

    changes

--- src/components/home-main/home-hero/main-hero.tsx
+++ src/components/home-main/home-hero/main-hero.tsx
@@ -58,7 +58,11 @@ export default function MainHero() {
                     height={798}
                     alt="Amrk Restaurant Systems"
                     placeholder="blur"
+<<<<<<< HEAD
                     priority={true}
+=======
+                    priority
+>>>>>>> 0d9ce5a (changes)
                 />
             </div>
         </div>

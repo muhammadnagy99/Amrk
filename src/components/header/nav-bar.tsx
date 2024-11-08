c7d200178cf71d6a39a237bcc7927118e2e1e8b5
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-08 20:02:32 +0200

    nav colors updates

--- src/components/header/nav-bar.tsx
+++ src/components/header/nav-bar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
     return(
         <div className="flex justify-center w-7/12" aria-labelledby="Amrk-NavBar">
             <nav className="flex w-special-nav justify-between">
-                <a href="" className="flex items-center mx-2 gap-[8px]">
+                <a href="" className="flex items-center mx-2 gap-[8px] text-primText">
                     حلول أمرك   
                     <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 1.00012L6 6.00049L1 1.00012ZM6 6.00049L11 1.00012L6 6.00049Z" fill="#23314C"/>
@@ -12,11 +12,11 @@ export default function NavBar() {
                     </svg>
                 </a>
 
-                <a href="/business-types" className="flex items-center mx-2">
+                <a href="/business-types" className="flex items-center mx-2  text-primText">
                     قطاعات نخدمها
                 </a>
 
-                <a href="" className="flex items-center mx-2 gap-[8px]">
+                <a href="" className="flex items-center mx-2 gap-[8px]  text-primText">
                     الموارد
                     <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M1 1.00012L6 6.00049L1 1.00012ZM6 6.00049L11 1.00012L6 6.00049Z" fill="#23314C"/>
@@ -24,7 +24,7 @@ export default function NavBar() {
                     </svg>
                 </a>
 
-                <a href="/pricing" className="flex items-center mx-2">
+                <a href="/pricing" className="flex items-center mx-2  text-primText">
                     الباقات
                 </a>
             </nav>

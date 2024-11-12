1b7c86556ca9edd01e6f41e9d807ce83ee7acc77
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-12 15:03:13 +0200

    business type page responsiveness

--- src/components/business.-types/details-components/bullet-points.tsx
+++ src/components/business.-types/details-components/bullet-points.tsx
@@ -4,9 +4,9 @@ import { BusinessBulletPointsProps } from "@/src/types/interfaces";
 
 export default function BusinessBulletPoints({ heading, listItems }: BusinessBulletPointsProps) {
     return (
-        <div className="flex flex-row justify-between rounded-[24px] p-[72px] border border-solid border-border1" aria-labelledby="Features-Bullets-List">
-            <div className="flex flex-col justify-center h-auto w-full gap-[32px]">
-                <h3 className="text-[32px] font-bold text-primText">
+        <div className="flex flex-row justify-between rounded-[24px] p-7 md:p-[72px] border border-solid border-border1" aria-labelledby="Features-Bullets-List">
+            <div className="flex flex-col justify-center h-auto w-full gap-4 md:gap-8">
+                <h3 className="text-base md:text-[32px] font-medium md:font-bold text-primText md:leading-[3.5rem]">
                     {heading.text.split(heading.highlited).map((part, index) => (
                         <React.Fragment key={index}>
                             {part}
@@ -18,11 +18,11 @@ export default function BusinessBulletPoints({ heading, listItems }: BusinessBul
                         </React.Fragment>
                     ))}
                 </h3>
-                <ul className="flex flex-col gap-4 pr-10">
+                <ul className="flex flex-col gap-4 pr-6 md:pr-10">
                     {listItems.map((item, index) => (
                         <li key={index} className="flex flex-row items-center gap-4">
                             <RightIcon2 />
-                            <p className="text-xl font-normal text-primText">
+                            <p className="text-xs md:text-xl font-normal text-primText">
                                 {item}
                             </p>
                         </li>

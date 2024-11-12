0d38bee4d9a95f4fb79ff5d58fc622bf32b443ce
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-12 14:07:40 +0200

    pricing show more and less

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,21 +1,48 @@
+"use client"
+
+import { useState } from 'react';
+import MinusIcon from "@/src/components/assets/minus-icon";
 import PageHeader from "@/src/components/assets/page-header";
+import PrimPlusIcon from "@/src/components/assets/primary-plus";
 import AccordionContainer from "@/src/components/pricing/accordion-container";
 import PackagesCards from "@/src/components/pricing/packages-cards";
 import PromoSection from "@/src/components/promotion-section/promotion-section";
 
-
 export default function PricingPage() {
-    return(
+    // State to manage the collapse state
+    const [isCollapsed, setIsCollapsed] = useState(true);
+
+    // Function to toggle the collapse state
+    const toggleCollapse = () => {
+        setIsCollapsed(!isCollapsed);
+    };
+
+    return (
         <section className="flex flex-col justify-center items-center w-full" aria-labelledby="Amrk-Business-Types">
             <div className="flex flex-col w-[88%] xl:max-w-[1200px] justify-between gap-10 mt-10 mb-10 md:gap-[80px] md:mt-[120px] md:mb-[40px]">
                 <PageHeader
-                        title="باقات مرنة لدعم مستقبل مطبخك"
-                        description="ابدأ مجاناً مع إمكانية إضافة الأدوات حسب الحاجة، أو اختر من خططنا الشاملة لتلبية جميع احتياجات مطبخك من البداية."
+                    title="باقات مرنة لدعم مستقبل مطبخك"
+                    description="ابدأ مجاناً مع إمكانية إضافة الأدوات حسب الحاجة، أو اختر من خططنا الشاملة لتلبية جميع احتياجات مطبخك من البداية."
                 /> 
 
                 <PackagesCards />
 
-                <AccordionContainer />
+                <div className="flex flex-row justify-end w-full">
+                    <button 
+                        onClick={toggleCollapse} 
+                        className="flex flex-row items-center pl-[24px] text-PrimBtn gap-4"
+                    >
+                        <span className="text-base font-medium">
+                            {isCollapsed ? 'عرض المزيد' : 'عرض أقل'}
+                        </span>
+                        {isCollapsed ? <PrimPlusIcon /> : <MinusIcon />}
+                    </button>
+                </div>
+
+                {/* Outer div to control the AccordionContainer display */}
+                <div className={`transition-all duration-300 ${isCollapsed ? 'max-h-0 overflow-hidden' : 'max-h-[1000px]'} w-full`}>
+                    <AccordionContainer />
+                </div>
             </div>
             <PromoSection />
         </section>

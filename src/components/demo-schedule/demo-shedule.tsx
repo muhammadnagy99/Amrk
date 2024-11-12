af08182797082ba9f78b82dcc3b61809c16370da
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-12 14:41:13 +0200

    Schedule Form

--- src/components/demo-schedule/demo-shedule.tsx
+++ src/components/demo-schedule/demo-shedule.tsx
@@ -4,9 +4,9 @@ import DemoForm from "./demo-form";
 import ContactUs from "./contact-us";
 export default function DemoSchedule() {
     return(
-        <section className="flex flex-col relative justify-start items-center w-full h-[876px] mb-[100px]">
+        <section className="flex flex-col relative justify-start items-center w-full md:h-[876px] mb-10 md:mb-[100px]">
             <div className="flex items-start justify-end w-full h-full absolute" aria-labelledby="demo-form-background">
-                <div className="xl:w-[50%] w-[550px] background-1 h-[750px] absolute z-0">
+                <div className="w-full xl:w-[50%] md:w-[550px] background-1 h-[570px] md:h-[750px] absolute z-0 bottom-[270px] md:bottom-auto">
                     <Image 
                         src={BG}
                         height={750}
@@ -17,15 +17,15 @@ export default function DemoSchedule() {
                     />
                 </div>
             </div>
-            <div className="flex flex-col md:w-[95%] w-special justify-center items-start gap-[80px] z-10 mt-[80px]" aria-labelledby="Amrk-Demo-Schedule-Form-Body">
+            <div className="flex flex-col w-[88%] md:max-w-[1200px] justify-center items-start gap-10 md:gap-[80px] z-10 mt-10 md:mt-[80px]" aria-labelledby="Amrk-Demo-Schedule-Form-Body">
 
-                <div className="flex flex-col w-full h-[672px] gap-[24px]" aria-labelledby="Amrk-Demo-Schedule-Form">
-                    <div className="flex flex-row w-full h-[574px] gap-[24px]">
-                        <div className="flex flex-col justify-center items-start gap-[24px] h-full" aria-labelledby="Amrk-Demo-Schedule-Form-Heading">
-                            <h2 className="text-5xl font-bold text-primText">
+                <div className="flex flex-col w-full md:h-[672px] gap-[24px]" aria-labelledby="Amrk-Demo-Schedule-Form">
+                    <div className="flex flex-col md:flex-row w-full md:h-[574px] gap-[24px]">
+                        <div className="flex flex-col justify-center items-start gap-4 md:gap-[24px] h-full" aria-labelledby="Amrk-Demo-Schedule-Form-Heading">
+                            <h2 className="w-full text-xl md:text-5xl font-bold text-primText">
                                 استمتع ببساطة أمـركـ    
                             </h2>
-                            <p className="text-2xl text-primText font-normal">
+                            <p className="w-full text-sm md:text-2xl text-primText font-normal">
                                 جرب نظامنا الآن واختبر بنفسك سهولة استخدامه.
                                 <br />
                                  فريقنا يسعد بالإجابة على استفساراتك في أي وقت.
@@ -44,7 +44,7 @@ export default function DemoSchedule() {
                     <h3 className="text-primText font-semibold text-xl">
                         سياسة الخصوصية                    
                     </h3>
-                    <p className="flex flex-row items-center text-xl gap-4">
+                    <p className="flex flex-row items-center text-base md:text-xl gap-4">
                         تلتزم أمرك بحماية خصوصيتك. لن نقوم بأي حال من الأحوال بمشاركة معلوماتك الشخصية مع أي طرف ثالث دون موافقتك.
                     </p>
                 </article>

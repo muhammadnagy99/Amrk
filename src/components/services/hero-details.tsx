ce0be5857abc1518d397dc17e97b49b4f7837bc4
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-12 16:31:37 +0200

    animation update v0.02

--- src/components/services/hero-details.tsx
+++ src/components/services/hero-details.tsx
@@ -1,6 +1,5 @@
-"use client";
 import PrimaryBtn from "../buttons/primary-button";
-import { motion } from "framer-motion";
+import HeroDescription from "./hero-heading";
 
 interface HeroDetails {
   heading: string;
@@ -9,13 +8,13 @@ interface HeroDetails {
 
 export default function HeroDetails({ heading, description }: HeroDetails) {
   const variants = {
-    hidden: { opacity: 0, y: 50 },
+    hidden: { opacity: 0, y: 25 },
     visible: (i: number) => ({
       opacity: 1,
       y: 0,
       transition: {
-        delay: i * 0.2,
-        duration: 0.7,
+        delay: i * 0.1,
+        duration: 0.3,
         ease: "easeOut",
       },
     }),
@@ -23,28 +22,21 @@ export default function HeroDetails({ heading, description }: HeroDetails) {
 
   return (
     <div className="flex flex-col justify-center h-auto w-full md:w-1/2 gap-10 md:gap-[64px]">
-      <motion.article 
+      <article 
         className="flex flex-col gap-2 md:gap-[24px]"
-        initial="hidden"
-        animate="visible"
-        custom={0}
-        variants={variants}
         >
 
         <h1 className="text-xs md:text-2xl font-normal text-primText">{heading}</h1>
-        <p className="text-2xl md:text-[40px] leading-8 md:leading-[3rem] font-bold text-primText">{description}</p>
-      </motion.article>
+        <HeroDescription text={description} />
+        
+      </article>
 
-      <motion.a 
+      <a 
         href="/demo-schedule" 
         className="inline-flex w-full md:w-1/3"
-        initial="hidden"
-        animate="visible"
-        custom={1}
-        variants={variants}
         >
         <PrimaryBtn text="احجز موعد للتجربة" />
-      </motion.a>
+      </a>
     </div>
   );
 }
---
+++ src/components/services/hero-heading.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+interface props {
+  text: string
+}
+
+export default function HeroDescription({ text }: props) {
+  const headingText = text;
+
+  const words = headingText.split(/(\s|\n)/);
+
+  const wordAnimation = {
+    hidden: { opacity: 0, y: 20 },
+    visible: (i: number) => ({
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: i * 0.1,
+        duration: 0.5,
+        ease: "easeOut",
+      },
+    }),
+  };
+
+  return (
+    <p className="flex flex-wrap text-2xl md:text-[40px] leading-8 md:leading-[3rem] font-bold text-primText">
+      {words.map((word, index) => (
+        <motion.span
+          key={index}
+          className={` inline-block mr-1 ${word === "\n" ? "w-full" : ""}`}
+          custom={index}
+          initial="hidden"
+          animate="visible"
+          variants={wordAnimation}
+        >
+          {word}
+        </motion.span>
+      ))}
+    </p>
+  );
+}

4bab25f3c56e6780a624d688dbfd736679325e90
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-17 07:42:11 +0200

    ..

--- src/components/header/btn-group.tsx
+++ src/components/header/btn-group.tsx
@@ -19,7 +19,7 @@ export default function BtnGroup({ data }: BtnGroupProps) {
     const { buttons, icon } = data;
 
     return (
-        <div className="hidden xl:flex items-center justify-end gap-6 w-btn-group lg:w-[40%]">
+        <div className="hidden xl:flex items-center justify-end gap-6 w-btn-group xl:w-[40%]">
             {buttons.map((button, index) => (
                 <a key={index} href={button.url} className={`${button.type === 'primary' ? 'w-[156px]' : 'w-[131px]'}`}>
                     {button.type === 'primary' ? (

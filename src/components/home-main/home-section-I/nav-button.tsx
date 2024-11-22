c3c7bb837a0d9e60ed72a09287daf7e9b268243d
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-11-22 20:45:59 +0200

    update styling

--- src/components/home-main/home-section-I/nav-button.tsx
+++ src/components/home-main/home-section-I/nav-button.tsx
@@ -13,7 +13,7 @@ interface NavButtonProps {
 const NavButton: React.FC<NavButtonProps> = ({ id, label, iconLight, iconDark, isActive, onClick }) => {
     return (
         <button
-            className={`flex flex-row justify-start items-center rounded-[8px] w-full lg:w-[317px] h-[40px] md:h-[64px] pl-4 pr-4 ${
+            className={`flex flex-row justify-start items-center rounded-[8px] w-full lg:w-[320px] h-[40px] md:h-[64px] pl-4 pr-4 ${
                 isActive ? 'bg-primText text-white border border-solid border-white' : 'bg-white text-primText'
             } gap-4`}
             onClick={() => onClick(id)}

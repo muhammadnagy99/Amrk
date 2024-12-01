e6bcd849a62decc66be5a746345be6dd83633ade
Muhammad Nagy <muhammadnagy71@gmail.com>
2024-12-01 19:46:53 +0200

    final updates

--- src/components/certifications/heading.tsx
+++ src/components/certifications/heading.tsx
@@ -7,7 +7,7 @@ const CertificationHeading: React.FC<CertificationHeadingProps> = ({ title, desc
     return (
         <div className="flex flex-col gap-4 md:gap-[40px] w-full">
             <div className="flex flex-col items-center gap-[24px] w-full">
-                <h1 className="flex items-center text-center text-2xl pr-7 pl-7 xl:text-[40px] md:text-4xl font-bold text-primText leading-[1.5]">
+                <h1 className="flex items-center text-center text-2xl md:pr-7 md:pl-7 xl:text-[40px] md:text-4xl font-bold text-primText leading-[1.5]">
                     {title}
                 </h1>
                 <p className="flex items-center text-primText font-normal leading-normal text-sm md:text-2xl text-center md:pr-40 md:pl-40">
